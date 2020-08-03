cb96b9efded188a5fd76585b995839f728083008
rlwing <rob@wingjr.com>
2020-08-03 13:24:41 -0700

    Remove unnecessary test

--- src/test/java/com/galvanize/bowling/AppTest.java
+++ src/test/java/com/galvanize/bowling/AppTest.java
@@ -3,12 +3,6 @@
  */
 package com.galvanize.bowling;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+
 }
