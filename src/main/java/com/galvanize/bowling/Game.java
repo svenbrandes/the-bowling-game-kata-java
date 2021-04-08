e3192718976fbb2b35b334ba85e68d849804a8c0
svenbrandes <sven.brandes@me.com>
2021-04-08 16:28:42 +0200

    implemented tests, all are passing

--- src/main/java/com/galvanize/bowling/Game.java
+++ src/main/java/com/galvanize/bowling/Game.java
@@ -1,4 +1,121 @@
 package com.galvanize.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Game {
+
+    private List<Frame> frameList = new ArrayList<>();
+
+    public Game() {
+        this.initGame();
+    }
+
+    private void initGame() {
+        for(int i = 1; i <= 10; i++) {
+            this.frameList.add(new Frame(i));
+        }
+    }
+
+    public void roll(int currentRoll) {
+        Frame currentFrame = this.frameList.stream().filter(frame -> !frame.isFinished()).findFirst().orElse(null);
+        if(currentFrame != null) {
+            currentFrame.addScore(currentRoll);
+            this.calcPrevSpareOrStrike(currentFrame);
+        }
+    }
+
+    private void calcPrevSpareOrStrike(Frame currentFrame) {
+        Frame previousFrame = this.getPreviousFrame(currentFrame);
+        if(previousFrame != null && currentFrame.throwCount != 3) {
+            if(currentFrame.getThrowCount() == 1) {
+                if (previousFrame.isSpare()) {
+                    previousFrame.addScore(currentFrame.getScore());
+                } else if (previousFrame.isStrike()) {
+                    calcPrevPrevStrike(currentFrame);
+                }
+            }
+            if(currentFrame.isFinished() || currentFrame.throwCount == 2) {
+                if(previousFrame.isStrike()) {
+                    previousFrame.addScore(currentFrame.getScore());
+                }
+            }
+        }
+    }
+
+    private void calcPrevPrevStrike(Frame currentFrame) {
+        Frame previousFrame = this.getPreviousFrame(currentFrame);
+        Frame prevPreviousFrame = previousFrame != null ? this.getPreviousFrame(previousFrame) : null;
+        if(prevPreviousFrame != null) {
+            prevPreviousFrame.addScore(currentFrame.getScore());
+        }
+    }
+
+    private Frame getPreviousFrame(Frame currentFrame) {
+        return this.frameList.stream()
+                .filter(frame -> frame.getId() == currentFrame.getId()-1).findFirst().orElse(null);
+    }
+
+    public int getTotalScore() {
+        int totalScore = this.frameList.stream().map(frame -> frame.getScore()).reduce(0, (a,b) -> a+b);
+        return totalScore;
+    }
+
+    private class Frame {
+        private int id;
+        private int score = 0;
+        private int throwCount = 0;
+        private boolean strike;
+        private boolean spare;
+        private boolean finished;
+
+        public Frame(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public int getScore() {
+            return this.score;
+        }
+
+        public int getThrowCount() {
+            return this.throwCount;
+        }
+
+        public boolean isStrike() {
+            return this.strike;
+        }
+
+        public boolean isSpare() {
+            return this.spare;
+        }
+
+        public boolean isFinished() {
+            return this.finished;
+        }
+
+        public void addScore(int score) {
+            this.score += score;
+            if(!this.isFinished()) {
+                this.throwCount++;
+                if(this.score == 10) {
+                    if(this.throwCount == 1) {
+                        this.strike = true;
+                    } else {
+                        this.spare = true;
+                    }
+                }
+                if((this.throwCount == 2 || this.strike) && this.id != 10) {
+                    this.finished = true;
+                } else if (this.throwCount == 3 || (this.throwCount == 2 && !(this.strike || this.spare))) {
+                    this.finished = true;
+                }
+            }
+        }
+    }
+
 }
--- src/test/java/com/galvanize/bowling/GameTest.java
+++ src/test/java/com/galvanize/bowling/GameTest.java
@@ -1,5 +1,118 @@
 package com.galvanize.bowling;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class GameTest {
+    Game game;
+
+    @BeforeEach
+    void setup() {
+        game = new Game();
+    }
+
+    @Test
+    void gameStarts() {
+        assertNotNull(game);
+    }
+
+    @Test
+    void checkScoreNewGame_rtnZero() {
+        int expected = 0;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rollBallFor2_rtnTwo() {
+        game.roll(2);
+        int expected = 2;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void roll20BallsFor0_rtnZero() {
+        for(int i=0; i<20; i++) {
+            game.roll(0);
+        }
+        int expected = 0;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void roll20BallsFor1_rtnTwenty() {
+        for(int i=0; i<20; i++) {
+            game.roll(1);
+        }
+        int expected = 20;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void roll20BallsWith1Spare_rtn19() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        game.roll(3);
+        for(int i=0; i<16; i++) {
+            game.roll(0);
+        }
+        int expected = 19;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void roll20BallsWith1Strike_rtn22() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(3);
+        for(int i=0; i<16; i++) {
+            game.roll(0);
+        }
+        int expected = 22;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void roll3Strikes_rtn60() {
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        for(int i=0; i<14; i++) {
+            game.roll(0);
+        }
+        int expected = 60;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void roll9Strikes_rtn240() {
+        for(int i=0; i<9; i++) {
+            game.roll(10);
+        }
+        game.roll(0);
+        game.roll(0);
+        int expected = 240;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rollAllStrikes_rtn300() {
+        for(int i=0; i<12; i++) {
+            game.roll(10);
+        }
+        int expected = 300;
+        int actual = game.getTotalScore();
+        assertEquals(expected, actual);
+    }
 
 }
